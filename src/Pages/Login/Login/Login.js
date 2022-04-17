c6da2da87ed070d09381eba25c14f495ef27a1b9
shakhawat-hridoy <shakhawat.hridoy56@gmail.com>
2022-04-17 17:30:36 +0600

    login logout form

--- src/Pages/Login/Login/Login.js
+++ src/Pages/Login/Login/Login.js
@@ -1,9 +1,73 @@
-import React from "react";
+import React, { useRef } from "react";
+import { Button, Form } from "react-bootstrap";
+import { useSignInWithEmailAndPassword } from "react-firebase-hooks/auth";
+import { useLocation, useNavigate } from "react-router-dom";
+import auth from "../../../firebase.init";
 
 const Login = () => {
+  const emailRef = useRef("");
+  const passwordRef = useRef("");
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  let from = location.state?.from?.pathname || "/";
+
+  const [signInWithEmailAndPassword, user, loading, error] =
+    useSignInWithEmailAndPassword(auth);
+
+  if (user) {
+    navigate(from, { replace: true });
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const email = emailRef.current.value;
+    const password = passwordRef.current.value;
+
+    signInWithEmailAndPassword(email, password);
+  };
+
+  const navigateRegister = () => {
+    navigate("/register");
+  };
+
   return (
-    <div>
-      <h2>Login</h2>
+    <div className="container w-50 mx-auto my-5">
+      <h2 className="text-primary text-center">Please Login</h2>
+      <Form onSubmit={handleSubmit}>
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Label>Email address</Form.Label>
+          <Form.Control
+            ref={emailRef}
+            type="email"
+            placeholder="Enter email"
+            required
+          />
+        </Form.Group>
+
+        <Form.Group className="mb-3" controlId="formBasicPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control
+            ref={passwordRef}
+            type="password"
+            placeholder="Password"
+            required
+          />
+        </Form.Group>
+        <Button variant="primary" type="submit">
+          Log in
+        </Button>
+      </Form>
+      <p>
+        New to Photographer?{" "}
+        <span
+          style={{ cursor: "pointer" }}
+          className="text-primary"
+          onClick={navigateRegister}
+        >
+          Please Register
+        </span>
+      </p>
     </div>
   );
 };
--- src/Pages/Login/Register/Register.js
+++ src/Pages/Login/Register/Register.js
@@ -1,11 +1,73 @@
-import React from "react";
+import React, { useRef } from "react";
+import { Button, Form } from "react-bootstrap";
+import { useCreateUserWithEmailAndPassword } from "react-firebase-hooks/auth";
+import { useNavigate } from "react-router-dom";
+import auth from "../../../firebase.init";
 
-const Register = () => {
+const Login = () => {
+  const nameRef = useRef("");
+  const emailRef = useRef("");
+  const passwordRef = useRef("");
+  const navigate = useNavigate();
+
+  const [createUserWithEmailAndPassword, user, loading, error] =
+    useCreateUserWithEmailAndPassword(auth);
+
+  const handleRegister = (event) => {
+    event.preventDefault();
+    const email = emailRef.current.value;
+    const password = passwordRef.current.value;
+
+    createUserWithEmailAndPassword(email, password);
+  };
+
+  const navigateLogin = () => {
+    navigate("/login");
+  };
+
   return (
-    <div>
-      <h2>Register</h2>
+    <div className="container w-50 mx-auto my-5">
+      <h2 className=" text-center">Please Register</h2>
+      <Form onSubmit={handleRegister}>
+        <Form.Group className="mb-3" controlId="formBasicName">
+          <Form.Label>Your name</Form.Label>
+          <Form.Control ref={nameRef} type="text" placeholder="Enter Name" />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Label>Email address</Form.Label>
+          <Form.Control
+            ref={emailRef}
+            type="email"
+            placeholder="Enter email"
+            required
+          />
+        </Form.Group>
+
+        <Form.Group className="mb-3" controlId="formBasicPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control
+            ref={passwordRef}
+            type="password"
+            placeholder="Password"
+            required
+          />
+        </Form.Group>
+        <Button variant="dark" type="submit">
+          Register
+        </Button>
+      </Form>
+      <p>
+        Already have an account?{" "}
+        <span
+          style={{ cursor: "pointer" }}
+          className="text-primary"
+          onClick={navigateLogin}
+        >
+          Please Login
+        </span>
+      </p>
     </div>
   );
 };
 
-export default Register;
+export default Login;
